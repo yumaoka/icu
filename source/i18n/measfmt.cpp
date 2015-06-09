8774007ff56fcb11bf7f9d73ca0d7842a2d6c8e7
John Emmons <emmo@us.ibm.com>
2015-06-09 07:17:32 +0000

    ICU-11728 Fix some copyrights
    X-SVN-Rev: 37505

--- source/i18n/measfmt.cpp
+++ source/i18n/measfmt.cpp
@@ -1,6 +1,6 @@
 /*
 **********************************************************************
-* Copyright (c) 2004-2014, International Business Machines
+* Copyright (c) 2004-2015, International Business Machines
 * Corporation and others.  All Rights Reserved.
 **********************************************************************
 * Author: Alan Liu
--- source/test/cintltst/cnumtst.c
+++ source/test/cintltst/cnumtst.c
@@ -1,6 +1,6 @@
 /********************************************************************
  * COPYRIGHT:
- * Copyright (c) 1997-2014, International Business Machines Corporation and
+ * Copyright (c) 1997-2015, International Business Machines Corporation and
  * others. All Rights Reserved.
  ********************************************************************/
 /********************************************************************************
