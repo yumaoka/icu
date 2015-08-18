c72cb0c6527072ea0b569e34fe4d99e29c24ecfd
John Emmons <emmo@us.ibm.com>
2015-08-18 21:42:16 +0000

    ICU-11846 Convert am-pm aliases
    X-SVN-Rev: 37784

--- source/i18n/numsys.cpp
+++ source/i18n/numsys.cpp
@@ -1,6 +1,6 @@
 /*
 *******************************************************************************
-* Copyright (C) 2010-2013, International Business Machines Corporation and
+* Copyright (C) 2010-2015, International Business Machines Corporation and
 * others. All Rights Reserved.
 *******************************************************************************
 *
