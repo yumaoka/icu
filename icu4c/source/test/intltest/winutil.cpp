72fdde687938d3b8f43cc67a3f6d8e9be5c9acf3
Shawn Steele <Shawn.Steele@microsoft.com>
2017-03-13 19:29:55 +0000

    ICU-13027 - Comments
    X-SVN-Rev: 39775

--- icu4c/source/test/intltest/winutil.cpp
+++ icu4c/source/test/intltest/winutil.cpp
@@ -36,7 +36,7 @@ static Win32Utilities::LCIDRecord *lcidRecords = NULL;
 static int32_t lcidCount  = 0;
 static int32_t lcidMax = 0;
 
-// Note that this test will skip locale names and only hit locales with assigned LCIDs
+// TODO: Note that this test will skip locale names and only hit locales with assigned LCIDs
 BOOL CALLBACK EnumLocalesProc(LPSTR lpLocaleString)
 {
     char localeID[ULOC_FULLNAME_CAPACITY];
@@ -71,7 +71,7 @@ BOOL CALLBACK EnumLocalesProc(LPSTR lpLocaleString)
     return TRUE;
 }
 
-// Note that this test will skip locale names and only hit locales with assigned LCIDs
+// TODO: Note that this test will skip locale names and only hit locales with assigned LCIDs
 Win32Utilities::LCIDRecord *Win32Utilities::getLocales(int32_t &localeCount)
 {
     LCIDRecord *result;
